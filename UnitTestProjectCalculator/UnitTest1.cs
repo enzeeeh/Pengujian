865368d4e8f2e896d21ddaf4c1488c092f74f37d
enzeeeh <enzi.muzakki22@gmail.com>
2014-05-08 20:29:00 +0700

    Code UI Test and UI Test

---
+++ UnitTestProjectCalculator/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalculatorv1;
+
+namespace UnitTestProjectCalculator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        MainWindow main = new MainWindow();
+        [TestMethod]
+        public void TestMethodPenjumlahan()
+        {
+            double result = main.TestPenjumlahan(3, 1);
+            double expected = 4;
+
+            Assert.AreEqual(result, expected, "Penjumlahan salah");
+        }
+        [TestMethod]
+        public void TestMethodPengurangan()
+        {
+            double result = main.TestPengurangan(3, 1);
+            double expected = 2;
+
+            Assert.AreEqual(result, expected, "Pengurangan salah");
+        }
+        [TestMethod]
+        public void TestMethodPembagian()
+        {
+            double result = main.TestPembagian(4, 2);
+            double expected = 2;
+
+            Assert.AreEqual(result, expected, "Pembagian salah");
+        }
+        [TestMethod]
+        public void TestMethodPerkalian()
+        {
+            double result = main.TestPerkalian(4, 2);
+            double expected = 8;
+
+            Assert.AreEqual(result, expected, "Perkalian salah");
+        }
+    }
+}
